2247702aefe79d07c318a3232884fc02fd79f791
bnail89 <bnail89@gmail.com>
2016-07-25 16:26:31 -0500

    js updated

--- js/app.js
+++ js/app.js
@@ -1,22 +1,22 @@
 for(var i = 1; i <=100; ++i){
     
     if(i % 15 === 0){
-        document.write('FizzBuzz');
-        document.write('<br/>');
+        document.write("FizzBuzz");
+        document.write("<br/>");
     }
     
     else if(i % 3 === 0){
-        document.write('Fizz');
-        document.write('<br/>');
+        document.write("Fizz");
+        document.write("<br/>");
     }
     
     else if(i % 5 === 0){
-        document.write('Buzz');
-        document.write('<br/>');
+        document.write("Buzz");
+        document.write("<br/>");
     }
 
     else{
         document.write(i);
-        document.write('<br/>');
+        document.write("<br/>");
     }
 }
